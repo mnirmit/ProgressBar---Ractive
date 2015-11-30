af36f5451e2715e83387cad3a0ba07b97ce98f94
mnirmit <nirmit.mutalik@gmail.com>
2015-11-30 11:42:56 +0530

    ProgressBar-Ractive
    Updated Condition of should go more then 100 in all three bars. updated
    progressBar.css,ProgressBar.html,ProgressBAr.js

--- progressBar.js
+++ progressBar.js
@@ -21,18 +21,14 @@ var ractive = new Ractive({
     }
 });
 
-
-;
 ractive.on({
-
-
     plusTwentyfive: function() {
         var selectedValue = ractive.get('selectedProgress');
         if (selectedValue == 0) {
             var progress = ractive.get('progress');
             progress = progress + 25;
-            if (progress > 100) {
-                progress = 100;
+            if (progress > 300) {
+                progress = 300;
             }
             ractive.set('progress', progress);
         }
@@ -40,8 +36,8 @@ ractive.on({
         if (selectedValue == 1) {
             var progress1 = ractive.get('progress1');
             progress1 = progress1 + 25;
-            if (progress1 > 100) {
-                progress1 = 100;
+            if (progress1 > 300) {
+                progress1 = 300;
             }
             ractive.set('progress1', progress1);
         }
@@ -49,20 +45,20 @@ ractive.on({
         if (selectedValue == 2) {
             var progress2 = ractive.get('progress2');
             progress2 = progress2 + 25;
-            if (progress2 > 100) {
-                progress2 = 100;
+            if (progress2 > 300) {
+                progress2 = 300;
             }
             ractive.set('progress2', progress2);
         }
-
     },
+
     plusTen: function() {
         var selectedValue = ractive.get('selectedProgress');
         if (selectedValue == 0) {
             var progress = ractive.get('progress');
             progress = progress + 10;
-            if (progress > 100) {
-                progress = 100;
+            if (progress > 300) {
+                progress = 300;
             }
             ractive.set('progress', progress);
         }
@@ -70,8 +66,8 @@ ractive.on({
         if (selectedValue == 1) {
             var progress1 = ractive.get('progress1');
             progress1 = progress1 + 10;
-            if (progress1 > 100) {
-                progress1 = 100;
+            if (progress1 > 300) {
+                progress1 = 300;
             }
             ractive.set('progress1', progress1);
         }
@@ -79,12 +75,13 @@ ractive.on({
         if (selectedValue == 2) {
             var progress2 = ractive.get('progress2');
             progress2 = progress2 + 10;
-            if (progress2 > 100) {
-                progress2 = 100;
+            if (progress2 > 300) {
+                progress2 = 300;
             }
             ractive.set('progress2', progress2);
         }
     },
+
     minusTen: function() {
         var selectedValue = ractive.get('selectedProgress');
         if (selectedValue == 0) {
